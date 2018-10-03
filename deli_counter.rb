c6a3027c822833b05c7c701efc6d73391aaf88de
Tania Whiting <tanwhiting@aol.com>
2018-10-03 15:53:24 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -6,7 +6,7 @@ def line (katz_deli)
 end
 
 def take_a-number (katz_deli, name)
-  katz_deli.unshift(name)
+  katz_deli << name 
   puts "Welcome, #{name}. You are number #{katz_deli.size} in line."
 end
 
@@ -14,7 +14,7 @@ def now_serving (katz_deli)
   if katz_deli == []
     puts "There is nobody waiting to be served!"
   else
-    puts "Currently serving #{katz_deli[katz_deli.length - 1]}"
-    katz_deli.pop
+    puts "Currently serving #{katz_deli[0]}"
+    katz_deli.shift
   end
 end
