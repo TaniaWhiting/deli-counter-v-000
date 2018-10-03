99862353efae05a69efc5e83be1aa0a7cb5e4752
Tania Whiting <tanwhiting@aol.com>
2018-10-03 16:11:40 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,5 +1,16 @@
 katz_deli = []
 
+def line (katz_deli)  
+  if katz_deli == []
+  "The line is currently empty."
+  else
+     puts "The line is currently: "
+     katz.deli.each_with_index do |name, index|
+     "#{index}. #{name} " 
+   end
+  end
+end
+
 def take_a_number (katz_deli, name)
   katz_deli << name 
   puts "Welcome, #{name}. You are number #{katz_deli.size} in line."
@@ -14,13 +25,3 @@ def now_serving (katz_deli)
   end
 end
 
-def line (katz_deli)  
-  if katz_deli == []
-  "The line is currently empty."
-  else
-     puts "The line is currently: "
-     katz.deli.each_with_index do |name, index|
-     "#{index}. #{name} " 
-   end
-  end
-end
