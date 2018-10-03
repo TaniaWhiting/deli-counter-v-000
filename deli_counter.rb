f2022940655f2d1a8a00517c70ac3e02b3279fa3
Tania Whiting <tanwhiting@aol.com>
2018-10-03 16:09:40 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,6 +1,6 @@
 katz_deli = []
 
-def take_a-number (katz_deli, name)
+def take_a_number (katz_deli, name)
   katz_deli << name 
   puts "Welcome, #{name}. You are number #{katz_deli.size} in line."
 end
